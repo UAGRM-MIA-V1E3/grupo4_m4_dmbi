6a28c618622cfbab18275a332e4493418cf3cde6
Elmar Rodas Banegas <Elmarcinho23@gmail.com>
2025-07-20 16:31:18 -0400

    Se agrego procedimientos almacenados para el OLTP

---
+++ grupo4_m4_dmbi/Schema/Programability/Stored Procedures/GetShipperChangesByRowVersion.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[GetShipperChangesByRowVersion]
+(  
+   @startRow BIGINT, 
+   @endRow  BIGINT 
+)
+AS
+BEGIN
+	SELECT [ShipperID]
+		  ,[CompanyName]
+		  ,[Phone]
+	  FROM [Shippers]
+	  WHERE [rowversion] > CONVERT(ROWVERSION,@startRow) 
+	  AND [rowversion] <= CONVERT(ROWVERSION,@endRow)
+END
+GO
