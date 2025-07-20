41d093096d4837821c3729f11907ce5987187de5
DESKTOP-CHNA10D\milca <milcacentellas@gmail.com>
2025-07-19 23:05:28 -0400

    *202507192304 Modificacion de tablas de dimensiones y adicion de StaginCustomer

---
+++ grupo4_m4_dmbi/NortwindDW/Schemas/Staging/StagingCustomer.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[StagingCustomer]
+(
+	[customerSK] [int] NOT NULL,
+	[company_name] [varchar](40) NOT NULL,
+	[contact_name] [varchar](30) NOT NULL,
+	[contact_title] [varchar](50) NULL
+);
+GO
