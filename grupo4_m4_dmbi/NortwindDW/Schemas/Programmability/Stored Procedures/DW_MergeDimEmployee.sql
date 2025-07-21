cf25f768381cbe37b0828cd21e02f774e0016a4e
Elmar Rodas Banegas <Elmarcinho23@gmail.com>
2025-07-21 11:58:33 -0400

    202521071158 se agrego los procedimientos almacenados para el DW

---
+++ grupo4_m4_dmbi/NortwindDW/Schemas/Programmability/Stored Procedures/DW_MergeDimEmployee.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE [dbo].[DW_MergeDimEmployee]
+AS
+BEGIN
+
+	UPDATE de
+	SET [FirstName]      = se.[FirstName]
+	   ,[LastName]       = se.[LastName]
+	   ,[Title]          = se.[Title]
+	   ,[TitleOfCourtesy]= se.[TitleOfCourtesy]
+	   ,[BirthDate]      = se.[BirthDate]
+	   ,[HireDate]       = se.[HireDate]
+	   ,[Address]        = se.[Address]
+	   ,[City]           = se.[City]
+	   ,[Region]         = se.[Region]
+	   ,[PostalCode]     = se.[PostalCode]
+	   ,[Country]        = se.[Country]
+	   ,[HomePhone]      = se.[HomePhone]
+	   ,[Extension]      = se.[Extension]
+	   ,[Photo]          = se.[Photo]
+	   ,[Notes]          = se.[Notes]
+	   ,[ReportsTo]      = se.[ReportsTo]
+	FROM [dbo].[DimEmployee]        de
+	INNER JOIN [StagingEmployee] se ON (de.[EmployeeSK]=se.[EmployeeSK])
+END
+GO
---
+++ grupo4_m4_dmbi/NortwindDW/Schemas/Programmability/Stored Procedures/DW_MergeFactOrders.sql
@@ -0,0 +1,30 @@
+﻿CREATE PROCEDURE [dbo].[DW_MergeFactOrders]
+AS
+BEGIN
+
+	UPDATE dfo
+	SET [ProductSK]     = sfo.[ProductSK]
+	   ,[CustomerSK]    = sfo.[CustomerSK]
+	   ,[EmployeeSK]    = sfo.[EmployeeSK]
+	   ,[ShipperSK]     = sfo.[ShipperSK]
+	   ,[OrderDateSK]   = sfo.[OrderDateSK]
+	   ,[RequiredDateSK]= sfo.[RequiredDateSK]
+	   ,[ShippedDateSK] = sfo.[ShippedDateSK]
+	   ,[OrderDate]     = sfo.[OrderDate]
+	   ,[RequiredDate]  = sfo.[RequiredDate]
+	   ,[ShippedDate]   = sfo.[ShippedDate]
+	   ,[UnitPrice]     = sfo.[UnitPrice]
+	   ,[Quantity]      = sfo.[Quantity]
+	   ,[Discount]      = sfo.[Discount]
+	   ,[Freight]       = sfo.[Freight]
+	   ,[ShipName]      = sfo.[ShipName]
+	   ,[ShipAddress]   = sfo.[ShipAddress]
+	   ,[ShipCity]      = sfo.[ShipCity]
+	   ,[ShipRegion]    = sfo.[ShipRegion]
+	   ,[ShipPostalCode]= sfo.[ShipPostalCode]
+	   ,[ShipCountry]   = sfo.[ShipCountry]
+	FROM [dbo].[FactOrders]        dfo
+	INNER JOIN [StagingFactOrders] sfo ON (dfo.[OrderSK]=sfo.[OrderSK])
+END
+GO
+
