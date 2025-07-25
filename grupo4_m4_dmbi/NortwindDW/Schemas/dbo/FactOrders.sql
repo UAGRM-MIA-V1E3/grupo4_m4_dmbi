5cb24be7fe7f315031a1e2529b1417baf955b8ca
Elmar Rodas Banegas <Elmarcinho23@gmail.com>
2025-07-24 22:30:29 -0400

    202507242228 Agregación del ETL completo de las tablas correspondiente al diseño modelado

--- grupo4_m4_dmbi/NortwindDW/Schemas/dbo/FactOrders.sql
+++ grupo4_m4_dmbi/NortwindDW/Schemas/dbo/FactOrders.sql
@@ -1,17 +1,17 @@
 ﻿CREATE TABLE [dbo].[FactOrders]
 (
-    OrderSK [int] IDENTITY(1,1) NOT NULL  CONSTRAINT [PK_FactOrders] PRIMARY KEY,
-    [OrderID] [int] NOT NULL,
-    [ProductSK] [int] NOT NULL,
+    [OrderSK] [int] IDENTITY(1,1) NOT NULL  CONSTRAINT [PK_FactOrders] PRIMARY KEY,
+	[OrderID] [int] NOT NULL,
+	[OrderDateKey] [int] NOT NULL,
+	[RequiredDateKey] [int] NOT NULL,
+	[ShippedDateKey] [int] NOT NULL,
 	[CustomerSK] [int] NULL,
 	[EmployeeSK] [int] NULL,
     [ShipperSK] [int] NULL,
-	[OrderDateSK] [int] NULL,
-	[RequiredDateSK] [int] NULL,
-	[ShippedDateSK] [int] NULL,
 	[OrderDate] [datetime] NULL,
 	[RequiredDate] [datetime] NULL,
 	[ShippedDate] [datetime] NULL,
+	[ProductSK] [int] NULL,
     [UnitPrice] [money] NOT NULL,
 	[Quantity] [smallint] NOT NULL,
 	[Discount] [real] NOT NULL,
@@ -22,20 +22,19 @@
 	[ShipRegion] [nvarchar](15) NULL,
 	[ShipPostalCode] [nvarchar](10) NULL,
 	[ShipCountry] [nvarchar](15) NULL
-
 );
 GO
 
 ALTER TABLE [dbo].[FactOrders] ADD CONSTRAINT [FK_DimCustomer] FOREIGN KEY([CustomerSK]) REFERENCES [dbo].[DimCustomer] ([CustomerSK]);
 GO
 
-ALTER TABLE [dbo].[FactOrders] ADD CONSTRAINT [FK_DimDate_OrderDate] FOREIGN KEY([OrderDateSK]) REFERENCES [dbo].[DimDate] ([DateSK]);
+ALTER TABLE [dbo].[FactOrders] ADD CONSTRAINT [FK_DimDate_OrderDate] FOREIGN KEY([OrderDateKey]) REFERENCES [dbo].[DimDate] ([DateKey]);
 GO
 
-ALTER TABLE [dbo].[FactOrders] ADD CONSTRAINT [FK_DimDate_RequiredDate] FOREIGN KEY([RequiredDateSK]) REFERENCES [dbo].[DimDate] ([DateSK]);
+ALTER TABLE [dbo].[FactOrders] ADD CONSTRAINT [FK_DimDate_RequiredDate] FOREIGN KEY([RequiredDateKey]) REFERENCES [dbo].[DimDate] ([DateKey]);
 GO
 
-ALTER TABLE [dbo].[FactOrders] ADD CONSTRAINT [FK_DimDate_ShippedDate] FOREIGN KEY([ShippedDateSK]) REFERENCES [dbo].[DimDate] ([DateSK]);
+ALTER TABLE [dbo].[FactOrders] ADD CONSTRAINT [FK_DimDate_ShippedDate] FOREIGN KEY([ShippedDateKey]) REFERENCES [dbo].[DimDate] ([DateKey]);
 GO
 
 ALTER TABLE [dbo].[FactOrders] ADD CONSTRAINT [FK_DimProduct] FOREIGN KEY([ProductSK]) REFERENCES [dbo].[DimProduct] ([ProductSK]);
@@ -46,4 +45,6 @@ GO
 
 ALTER TABLE [dbo].[FactOrders] ADD CONSTRAINT [FK_DimShipper] FOREIGN KEY([ShipperSK]) REFERENCES [dbo].[DimShipper] ([ShipperSK]);
 GO
+
+
 
